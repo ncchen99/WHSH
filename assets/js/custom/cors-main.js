63a5b3b2787bc8d63873a47200150fe5d7d6f53d
ncchen99 <ncchen99@gmail.com>
2021-05-01 23:42:26 +0800

    modal, loading, queryNewsContent

--- assets/js/custom/cors-main.js
+++ assets/js/custom/cors-main.js
@@ -1,67 +1,133 @@
 const file_url = "./assets/js/custom/request_urls.yml";
 
+function get_data(file_url) {
+  var result;
+  $.ajax({
+    url: file_url,
+    type: "get",
+    async: false,
+    success: function (data) {
+      result = data;
+    },
+  });
+  return result;
+}
+
+class Widget {
+  constructor(file_url) {
+    this.file_url = file_url;
+    this.json_data = jsyaml.load(get_data(file_url));
+    this.base_url = this.json_data["base_url"];
+    this.ajax_server = this.json_data["ajax_server"];
+  }
+
+  query_news(board, tabs) {
+    $.ajax({
+      // headers: json_data['request_headers'],
+      type: this.json_data[board][tabs]["general"]["request_method"],
+      url:
+        this.ajax_server +
+        this.base_url +
+        this.json_data[board][tabs]["general"]["request_url"],
+      dataType: this.json_data[board][tabs]["general"]["data_type"],
+      data: this.json_data[board][tabs]["query_string_parameters"],
+
+      success: function (response) {
+        console.log(response);
+        $("#news-table-div").html(` 
+           <table class="table table-responsive" id="news-table">
+           </table>`);
+        var innerHTML = `<thead>
+          <tr>
+            <th class="text-center">時間</th>
+            <th>單位</th>
+            <th>標題</th>
+            <th>點閱</th>
+          </tr>
+          </thead>
+          <tbody>
+          </tbody>`;
+        $("#news-table").html(innerHTML);
+        response.shift();
+        for (const row of response) {
+          var tr = "<tr>";
+          tr += "<td>" + row["time"] + "</td>";
+          tr += "<td>" + row["unit_name"] + "</td>";
+          tr += "<td>";
+          tr +=
+            '<a href="javascript:void(0)" onclick="widget.query_news_content(';
+          tr += row["newsId"] + ", ";
+          tr +=
+            "'" + tabs + "'" + ')" class="btn btn-link text-primary news-btn">';
+          if (row["top"] == 1) {
+            tr += '<span class="badge badge-pill badge-danger">HOT</span>';
+          }
+          tr += " " + row["title"] + "</a>" + "</td>";
+          tr += "<td>" + row["clicks"] + "</td>";
+          tr += "</tr>";
+          $("#news-table > tbody").append(tr);
+        }
+      },
+      error: function (thrownError) {
+        $("#news-table-div").html(`<div class="alert alert-danger" role="alert">
+        ERROR :(
+        </div>`);
+        console.log(thrownError);
+      },
+    });
+  }
+
+  query_news_content(nid, tabs) {
+    var board = "query_news_content";
+    $("#news-modal-body").html(
+      `<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>`
+    );
+    $("#news-modal").modal("show");
+    $.ajax({
+      // headers: json_data['request_headers'],
+      type: this.json_data[board][tabs]["general"]["request_method"],
+      url:
+        this.ajax_server +
+        this.base_url +
+        this.json_data[board][tabs]["general"]["request_url"],
+      dataType: this.json_data[board][tabs]["general"]["data_type"],
+      data: {
+        ...this.json_data[board][tabs]["query_string_parameters"],
+        ...{ nid: nid },
+      },
+
+      success: function (response) {
+        console.log(response);
+        $("#news-modal-title").text(response[0]["title"]);
+        var innerHTML = "";
+        if (response[0]["attachedfile"] != "[]") {
+          innerHTML += `<h5>附件區</h5>
+          <a href="#" role="button" class="btn btn-link text-primary" title="附件" data-content="Popover body content is set in this attribute.">button</a>
+          <hr>`;
+        }
+        innerHTML += decodeURIComponent(response[0]["content"]);
+        $("#news-modal-body").html(innerHTML);
+      },
+      error: function (thrownError) {
+        $("#news-modal-title").text("ERROR ：(");
+        $("#news-modal-body")
+          .html(`<div class="alert alert-danger" role="alert">
+        ERROR :(
+        </div>`);
+        console.log(thrownError);
+      },
+    });
+  }
+}
+
+var widget;
+var board = "lower_board";
+var tabs = "bulletin_board";
+
 (function () {
   "use strict";
   $(document).ready(function () {
-    $.get(file_url).done(function (data) {
-      var json_data = jsyaml.load(data);
-
-      console.log(json_data);
-      const base_url = json_data["base_url"];
-      const ajax_server = json_data["ajax_server"];
-
-      var board = "lower_board";
-      var tabs = "bulletin_board";
-
-      $.ajax({
-        // headers: json_data['request_headers'],
-        type: json_data[board][tabs]["general"]["request_method"],
-        url:
-          ajax_server +
-          base_url +
-          json_data[board][tabs]["general"]["request_url"],
-        dataType: json_data[board][tabs]["general"]["data_type"],
-        data: json_data[board][tabs]["query_string_parameters"],
-
-        success: function (response) {
-          console.log(response);
-          $("#news-table-div").html(` 
-			<table class="table table-responsive" id="news-table">
-			</table>`);
-          var innerHTML = `<thead>
-			<tr>
-				<th class="text-center">時間</th>
-				<th>單位</th>
-				<th>標題</th>
-				<th>點閱</th>
-			</tr>
-			</thead>
-			<tbody>
-			</tbody>`;
-          $("#news-table").html(innerHTML);
-          response.shift();
-          for (const row of response) {
-            var tr = "<tr>";
-            tr += "<td>" + row["time"] + "</td>";
-            tr += "<td>" + row["unit_name"] + "</td>";
-            tr += "<td>";
-            if (row["top"] == 1) {
-              tr += '<span class="badge badge-pill badge-danger">HOT</span>';
-            }
-            tr += " " + row["title"] + "</td>";
-            tr += "<td>" + row["clicks"] + "</td>";
-            tr += "</tr>";
-            $("#news-table > tbody").append(tr);
-          }
-        },
-        error: function (thrownError) {
-          $("#news-table-div")
-            .html(`<div class="alert alert-danger" role="alert">
-					This is a danger alert—check it out!
-				  </div>`);
-          console.log(thrownError);
-        },
-      });
-    });
+    widget = new Widget(file_url);
+    widget.query_news(board, tabs);
   });
 })();
